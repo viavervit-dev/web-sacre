3ecc310da8fe6cbd6c425e13d7a1ea21f59ff907
NachoBasilio <ignacio.n.basilio.b@gmail.com>
2026-04-13 21:39:05 -0300

    add: path alias @/ mapeado a src/ en vite y typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./src/setupTests.ts'],
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'),
+		},
+	},
+	test: {
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: ['./src/setupTests.ts'],
+	},
+});
